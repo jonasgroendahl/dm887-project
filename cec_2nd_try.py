ee2367dd369e16eb66578c7cde4232dbea2f5ee9
jonasgroendahl <jonas.groendahl.pedersen@lego.com>
2024-04-18 08:37:57 +0200

    softmax temperature, termination condition check

--- cec_2nd_try.py
+++ cec_2nd_try.py
@@ -8,6 +8,32 @@ from torch import Tensor
 from torch.autograd import Variable
 from replay_memory import ReplayMemory
 import random
+import matplotlib.pyplot as plt
+
+# defaults from https://gymnasium.farama.org/environments/mujoco/hopper/
+healthy_state_range = (-100, 100)
+healthy_z_range = (0.7, float("inf"))
+healthy_angle_range = (-0.2, 0.2)
+
+
+def check_termination_condition_hopper(state):
+    # Check if any element of the observation is outside the healthy state range
+    observation = state[1:]
+
+    for value in observation:
+        if not (healthy_state_range[0] <= value <= healthy_state_range[1]):
+            return "Range issue"  # Hopper is unhealthy
+
+    # Check if the height of the hopper is outside the healthy z range
+    if not (healthy_z_range[0] <= state[0] <= healthy_z_range[1]):
+        return "Height issue"  # Hopper is unhealthy
+
+    # Check if the angle of the hopper is outside the healthy angle range
+    angle_index = 1
+    if not (healthy_angle_range[0] <= state[angle_index] <= healthy_angle_range[1]):
+        return "Angle issue"  # Hopper is unhealthy
+
+    return "Unsure"
 
 
 def find_k_closest_vectors(vector, existing_vectors, k=5):
@@ -21,10 +47,18 @@ def find_k_closest_vectors(vector, existing_vectors, k=5):
     return sorted_vectors
 
 
-def softmax(x):
+def softmax(x, temperature=1.0):
     """Compute softmax values for each sets of scores in x."""
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum(axis=0)
+
+
+# function to draw total_reward for each episode over time
+def draw(total_rewards):
+    plt.plot(total_rewards)
+    plt.ylabel("Total reward")
+    plt.xlabel("Episode")
+    plt.show()
 
 
 class Memory:  # Memory good for continuous action space
@@ -84,16 +118,50 @@ print("Latent space size: ", latent_space_size)
 replay_memory = ReplayMemory(5000)
 
 mem = Memory()
+
 # hyperparameters
-amount_of_training_to_do = 10000
+amount_of_training_to_do = 5000
+eval_loops = 300
 gamma = 0.99  # discount factor
-close_neighbour_threshold = 1
-noise_probability = 0.1
-noise_std_dev = 0.1
+close_neighbour_threshold_action_selection = (
+    0.5  # threshold for selecting close neighbours
+)
+close_neighbour_threshold_memory = 0.5  # threshold for updating memory
+noise_probability = 0.3  # higher value means more exploration (0-1), probability of adding noise to selected action
+noise_std_dev = (
+    0.2  # higher value means more exploration (0-1), more noise to selected action
+)
+softmax_temperature = 1.0  # higher value means more exploration (0-1), larger gap between values when calculating softmax
+
+# not in use right now
+hyperparameter_sets = [
+    {
+        "amount_of_training_to_do": 5000,
+        "eval_loops": 300,
+        "gamma": 0.99,
+        "close_neighbour_threshold_action_selection": 0.5,
+        "close_neighbour_threshold_memory": 0.5,
+        "noise_probability": 0.3,
+        "noise_std_dev": 0.2,
+        "softmax_temperature": 1.0,
+    },
+    {
+        "amount_of_training_to_do": 5000,
+        "eval_loops": 300,
+        "gamma": 0.99,
+        "close_neighbour_threshold_action_selection": 0.4,
+        "close_neighbour_threshold_memory": 0.4,
+        "noise_probability": 0.3,
+        "noise_std_dev": 0.3,
+        "softmax_temperature": 0.9,
+    },
+]
+
+total_rewards = []
+total_rewards_eval = []
+
 
 # CEC Implementation: https://arxiv.org/pdf/2211.15183.pdf
-
-
 # main loop
 def train():
     for i in range(amount_of_training_to_do):
@@ -104,11 +172,15 @@ def train():
         turns = 0
         episode_mem = EpisodeMemory()
 
+        reason_for_termination = None
+
         while not done:
             nearest_neighbours = mem.lookup(state)
             # filter off the far away neighbours
             filtered_neighbours = [
-                x for x in nearest_neighbours if x[0] < close_neighbour_threshold
+                x
+                for x in nearest_neighbours
+                if x[0] < close_neighbour_threshold_action_selection
             ]
 
             if len(filtered_neighbours) > 0:
@@ -117,17 +189,15 @@ def train():
                 values = np.array([x[1][2] for x in filtered_neighbours])
 
                 # Softmax action selection
-                action_probs = softmax(values)
+                action_probs = softmax(values, temperature=softmax_temperature)
                 selected_action_index = np.random.choice(
                     len(filtered_neighbours), p=action_probs
                 )
                 action = actions[selected_action_index]
 
                 # Add Gaussian noise to the selected action
-                if np.random.rand() < noise_probability:  # 0.1 - 0.5
-                    action += np.random.normal(
-                        0, noise_std_dev, size=action.shape
-                    )  # noise_std_dev 0.1
+                if np.random.rand() < noise_probability:
+                    action += np.random.normal(0, noise_std_dev, size=action.shape)
             else:
                 action = game.action_space.sample()
 
@@ -140,15 +210,17 @@ def train():
 
             done = terminated or truncated
 
-            if done:
-                if terminated:
-                    end_state = "terminated"
-                else:
-                    end_state = "truncated"
-
             turns += 1
 
             total_reward += reward
+
+            if done:
+                if truncated:
+                    end_state = "truncated"
+                else:
+                    end_state = "terminated"
+                reason_for_termination = check_termination_condition_hopper(state)
+                total_rewards.append(total_reward)
 
         v = 0
         while len(episode_mem) > 0:
@@ -166,7 +238,7 @@ def train():
             # if there is no close neighbour or the closest neighbour is far away
             if (
                 the_closest_state_neighbour is None
-                or the_closest_state_neighbour[0] > close_neighbour_threshold
+                or the_closest_state_neighbour[0] > close_neighbour_threshold_memory
             ):
                 # add the state to the memory, we are far away from the neighbours meaning we have new information
                 mem.add((state, action, v))
@@ -177,15 +249,12 @@ def train():
                     mem.update(the_closest_state_neighbour[1][0], (state, action, v))
 
         print(
-            f"Episode {i} after {turns} turns - {total_reward} - {end_state} - {len(mem)}"
+            f"Episode {i} after {turns} turns - {total_reward} - {end_state} - {len(mem)} - {reason_for_termination if reason_for_termination is not None else state}"
         )
 
 
-train()
-
-
 def evaluate():  # evaluation
-    for i in range(5):
+    for i in range(eval_loops):
         state, info = game.reset()
         done = False
         total_reward = 0
@@ -194,18 +263,14 @@ def evaluate():  # evaluation
 
         while not done:
             nearest_neighbours = mem.lookup(state)
-            # filter off the far away neighbours
-            filtered_neighbours = [
-                x for x in nearest_neighbours if x[0] < close_neighbour_threshold
-            ]
 
-            actions = np.array([x[1][1] for x in filtered_neighbours])
-            values = np.array([x[1][2] for x in filtered_neighbours])
+            actions = np.array([x[1][1] for x in nearest_neighbours])
+            values = np.array([x[1][2] for x in nearest_neighbours])
 
             # Softmax action selection
             action_probs = softmax(values)
             selected_action_index = np.random.choice(
-                len(filtered_neighbours), p=action_probs
+                len(nearest_neighbours), p=action_probs
             )
             action = actions[selected_action_index]
 
@@ -215,16 +280,23 @@ def evaluate():  # evaluation
 
             done = terminated or truncated
 
+            turns += 1
+
+            total_reward += reward
+
             if done:
                 if terminated:
                     end_state = "terminated"
                 else:
                     end_state = "truncated"
-
-            turns += 1
-
-            total_reward += reward
+                total_rewards_eval.append(total_reward)
 
         print(
-            f"Evaluation: Episode {i} after {turns} turns - {total_reward} - {end_state}"
+            f"Evaluation: Episode {i} after {turns} turns - {total_reward} - {end_state} - {len(mem)}"
         )
+
+
+train()
+evaluate()
+draw(total_rewards)
+draw(total_rewards_eval)
